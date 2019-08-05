e7188ff5d6f6a7b58a4fd4c9a601b45551870cee
Katie <kegolden78@gmail.com>
2019-08-05 13:27:15 -0500

    starting over

---
+++ src/main/java/com/project/collegePlanning/controller/CollegePlanningController.java
@@ -0,0 +1,23 @@
+package com.project.collegePlanning.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("college planning")
+public class CollegePlanningController {
+
+    @RequestMapping(value = "")
+    public String loadIndexPage(){
+        return "index.html";
+    }
+
+    @RequestMapping(value = "welcome")
+    public String loadWelcomePage(){
+        return "welcome.html";
+    }
+
+
+}
