a665dda82cf03b289dab380f667ad5f78da9984a
Katie <kegolden78@gmail.com>
2019-08-05 19:57:42 -0500

    controllers work on localhost:8080

--- src/main/java/com/project/collegePlanning/controller/CollegePlanningController.java
+++ src/main/java/com/project/collegePlanning/controller/CollegePlanningController.java
@@ -1,22 +1,38 @@
 package com.project.collegePlanning.controller;
 
 
+import com.project.collegePlanning.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
 
 @Controller
 @RequestMapping("college-planning")
 public class CollegePlanningController {
+
+    static ArrayList<User> users;
+
+    public CollegePlanningController(){
+        users = new ArrayList<User>();
+        users.add(new User("TimeLord", "Katie", "Golden", "kegolden@gmail.com"));
+    }
 
     @RequestMapping(value = "")
     public String loadIndexPage(){
         return "index.html";
     }
 
-    @RequestMapping(value = "welcome")
-    public String loadWelcomePage(){
+    @RequestMapping(value = "welcome", method = RequestMethod.GET)
+    public String getNewUser(){
         return "welcome.html";
     }
 
+    @RequestMapping(value = "welcome", method = RequestMethod.POST)
+    public String saveNewUser(@RequestParam String userName, @RequestParam String firstName, @RequestParam String lastName, @RequestParam String email){
+
+        return "welcome.html";
+    }
 }
---
+++ src/main/java/com/project/collegePlanning/model/User.java
@@ -0,0 +1,54 @@
+package com.project.collegePlanning.model;
+
+public class User {
+
+    private String userName;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+
+    public User(String userName, String firstName, String lastName, String email) {
+        setUserName(userName);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setEmail(email);
+
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        //if (email.contains("@")){
+        this.email = email;
+        //}
+    }
+
+
+}
